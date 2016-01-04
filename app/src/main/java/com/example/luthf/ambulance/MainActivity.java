5ddd417c08c13cc960ff56a2e9a0813897f2b8e7
luthfiapriyanto <luthfiapriyanto@gmail.com>
2016-01-04 10:26:46 +0700

    example

--- app/src/main/java/com/example/luthf/ambulance/MainActivity.java
+++ app/src/main/java/com/example/luthf/ambulance/MainActivity.java
@@ -55,18 +55,16 @@ public class MainActivity extends FragmentActivity {
         setUpMapIfNeeded();
 
         mMap.setMyLocationEnabled(true);
+        findViewById(R.id.email_sign_in_button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ETA.class));
+            }
+        });
 
 
+    }
 
-    @Override
-    findViewById(R.id.email_sign_in_button1).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            startActivity(new Intent(MainActivity.this, ETA.class));
-        }
-    });
-
-}
     protected void onResume() {
         super.onResume();
 
